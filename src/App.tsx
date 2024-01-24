9a2de2f8f604ea74540f717f8b91837ca314caa8
Roman Lipatov <rlipatov96@gmail.com>
2024-01-24 14:52:55 +0200

    BrowserRouter to Hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { PersonsList } from './components/PersonsList';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { FullInfo } from './components/FullInfo';
 import { Card } from 'react-bootstrap';
 
